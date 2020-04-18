43f9b45a18987c7cc70cba4bd7e0164b5382565a
AbhishekKr <abhikumar163@gmail.com>
2020-04-18 23:08:14 +0530

    update README; refactor tasks.sh for help option
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 ## lyrical-video-generator
 
-> to generate a lyrical video from provided audio file
+> to generate a lyrical video from provided audio file or text file
 
-### Utilities | **WIP**
+### Usage
+
+* syntax: `./tasks.sh <help|dep|txt-to-vid|t2v|frame-n-vid|fnv|dataframes|frames|vid|transcribe>`
+
+* hard-coded path as of now (WIP for config)
+
+> * base data dir at './lvg-data'
+>
+> * dir for audio files at './lvg-data/transcription'
+>
+> * mp3 audio file used as input at './lvg-data/transcription/transcript.mp3'
+>
+> * transcripted text file from audio at 'lvg-data/transcript.txt'
+>
+> * dataframe file created from text file at './lvg-data/transcript.txt.lvg'
+>
+> * image frames created from dataframes at './lvg-data/frames'
+>
+> * video name = '/tmp/mygeneratedvideo.avi'
+
+* Parameters Help
+
+> * dep: to install pip dependencies
+>
+> * transcribe: to convert audio file to text
+>
+> * dataframes: convert available transcription/text to input data format for create-frame
+>
+> * frames: to create frame images for video from dataframes
+>
+> * vid: to create video from existing frames
+>
+> * tnv (or txt-to-vid): to run all commands from creating dataframes to creating video (if uou have text file)
+>
+> * fnv (or frame-n-vid): to create frame images and video from it
+
+### Utilities
 
 * [transcribe.py](transcribe.py) to convert `lvg-data/transcription/transcript.mp3` into text at `lvg-data/transcript.txt`
 
--- tasks.sh
+++ tasks.sh
@@ -11,12 +11,37 @@ dependencies(){
     opencv-python
 }
 
+help_message(){
+    echo "usage: $0 <dep|txt-to-vid|t2v|frame-n-vid|fnv|dataframes|frames|vid|transcribe>"
+    echo ""
+    echo "Parameters Help"
+    echo "* dep: to install pip dependencies"
+    echo "* transcribe: to convert audio file to text"
+    echo "* dataframes: convert available transcription/text to input data format for create-frame"
+    echo "* frames: to create frame images for video from dataframes"
+    echo "* vid: to create video from existing frames"
+    echo "* tnv (or txt-to-vid): to run all commands from creating dataframes to creating video"
+    echo "* fnv (or frame-n-vid): to create frame images and video from it"
+}
+
 case $1 in
+  help)
+    help_message
+    ;;
   dep*)
     dependencies
     ;;
+  transcribe*)
+    python transcribe.py
+    ;;
   dataframes)
     python to-frame-data.py
+    ;;
+  frames)
+    python create-frames.py
+    ;;
+  vid)
+    python create-video.py ${PWD}/lvg-data/frames
     ;;
   txt-to-vid|t2v)
     set -ex
@@ -25,29 +50,15 @@ case $1 in
     python create-video.py ${PWD}/lvg-data/frames
     set +ex
     ;;
-  frames)
-    python create-frames.py
-    ;;
   frame-n-vid|fnv)
     set -ex
     python create-frames.py
     python create-video.py ${PWD}/lvg-data/frames
     set +ex
     ;;
-  vid)
-    python create-video.py ${PWD}/lvg-data/frames
-    ;;
   **)
     echo "unknown option: ${1:-empty_parameter_set}"
-    echo "usage: $0 <dep|txt-to-vid|t2v|frame-n-vid|fnv|dataframes|frames|vid>"
-    echo ""
-    echo "Parameters Help"
-    echo "* dep: to install pip dependencies"
-    echo "* dataframes: convert available transcription/text to input data format for create-frame"
-    echo "* frames: to create frame images for video from dataframes"
-    echo "* vid: to create video from existing frames"
-    echo "* tnv (or txt-to-vid): to run all commands from creating dataframes to creating video"
-    echo "* fnv (or frame-n-vid): to create frame images and video from it"
+    help_message
     exit 123
     ;;
 esac
